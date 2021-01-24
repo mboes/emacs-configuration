c2ef72bf58d56b0153c003ec5722344b8999e638
Mathieu Boespflug <m@tweag.io>
2021-01-24 19:53:10 +0200

    Switch to develop branch of straight.el

--- init.el
+++ init.el
@@ -33,7 +33,10 @@
  create-lockfiles nil
  make-backup-files nil)
 
+(setq straight-check-for-modifications nil)
+
 ;; Bootstrap straight.el.
+(setq straight-repository-branch "develop")
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
